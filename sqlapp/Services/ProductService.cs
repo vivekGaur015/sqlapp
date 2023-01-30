827358a271e4dc11885cbb8a55fb3bc611d8a6ed
vivekGaur015 <vivek.dstreetwalker@gmail.com>
2023-01-30 13:07:16 +0530

    Add project files.

---
+++ sqlapp/Services/ProductService.cs
@@ -0,0 +1,57 @@
+﻿using sqlapp.Models;
+using System.Data.SqlClient;
+
+namespace sqlapp.Services
+{
+    public class ProductService
+    {
+        private static string db_source = "appserver015.database.windows.net";
+        private static string db_user = "vivekgaur015";
+        private static string db_password = "ilu2mbsm!VG";
+        private static string db_database = "appdb";
+
+        private SqlConnection GetConnection()
+        {
+             var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = db_source;
+            _builder.UserID = db_user;
+            _builder.Password = db_password;
+            _builder.InitialCatalog = db_database;
+
+            return new SqlConnection(_builder.ConnectionString);
+        }
+
+        public List<Product> GetProducts()
+        {
+            SqlConnection conn = GetConnection();
+
+            List<Product> _product_list = new List<Product>();
+
+            string statement = "Select ProductID,ProductName,Quantity from Products";
+
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand(statement, conn);
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Product product = new Product()
+                    {
+                        ProductID = reader.GetInt32(0),
+                        ProductName = reader.GetString(1),
+                        Quantity = reader.GetInt32(2)
+                    };
+
+                    _product_list.Add(product);
+                }
+            }
+
+            conn.Close();
+            return _product_list;
+
+
+        }
+    }
+}
